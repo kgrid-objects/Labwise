47b96195673e1d08ec59e42678588091beb28722
circleci <kgrid-developers@umich.edu>
2019-01-10 12:26:27 -0500

    Move to collection directory and updated package script

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
-
-zip -r $1.zip $1
+# clean up only leaving zip files
+rm -r *.zip
+cd collection
+# create one big zip of all kos
+zip -r -X ../labwise-all.zip * -x \"*.DS_Store\" "*.zip*"
