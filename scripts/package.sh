f96d19e4b1e5e44573a66aad038b338f4b5867ba
Greg Farris <farrisg@umich.edu>
2019-01-14 13:35:48 -0500

    Added zip individual KOs

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,4 +3,11 @@
 rm -r *.zip
 cd collection
 # create one big zip of all kos
+# create zip for each ko
+
+for d in * ; do
+    echo "$d.zip"
+    zip -r -X "../$d.zip" $d -x \"*.DS_Store\"
+done
+
 zip -r -X ../labwise-all.zip * -x \"*.DS_Store\" "*.zip*"
